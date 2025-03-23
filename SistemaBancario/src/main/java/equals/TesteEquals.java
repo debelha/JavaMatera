3f6f329e25b9da7359bba5ca72a3d2041dac7b42
debelha <dleitedias@gmail.com>
2025-03-22 22:56:38 -0300

    aula 2: correção exemplo equals

--- SistemaBancario/src/main/java/equals/TesteEquals.java
+++ SistemaBancario/src/main/java/equals/TesteEquals.java
@@ -2,14 +2,15 @@ package equals;
 
 public class TesteEquals {
     public static void main(String[] args) {
-        String nome1 = "deborah";
+        String nome1 = "Deborah";
         String nome2 = "Deborah";
         String nome3 = new String("Deborah");
+        String nome4 = "Deborah";
 
         System.out.println("nome1 == nome2: " + (nome1 == nome2));
         System.out.println("nome1 == nome3: " + (nome1 == nome3));
         System.out.println("nome1.equals(nome2): " + nome1.equals(nome2));
         System.out.println("nome1.equals(nome3): " + nome1.equals(nome3));
-        System.out.println("nome1.equalsIgnoreCase(nome2): " + nome1.equalsIgnoreCase(nome2));
+        System.out.println("nome1.equalsIgnoreCase(nome4): " + nome1.equalsIgnoreCase(nome4));
     }
 }
