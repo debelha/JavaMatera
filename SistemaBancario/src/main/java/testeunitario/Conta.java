24192765f6eb9be6b9b96989cf88d8c73a43576d
debelha <dleitedias@gmail.com>
2025-03-25 17:02:33 -0300

    aula 4: testes unitários

---
+++ SistemaBancario/src/main/java/testeunitario/Conta.java
@@ -0,0 +1,23 @@
+package testeunitario;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Conta {
+    private Double saldo;
+    private Double taxaOperacao;
+
+    public void depositar(Double valor) {
+        this.saldo += valor - taxaOperacao;
+    }
+
+    public void sacar (Double valor) {
+        this.saldo -= valor + taxaOperacao;
+    }
+
+
+}
---
+++ SistemaBancario/src/test/java/testesunitarios/ContaTest.java
@@ -0,0 +1,19 @@
+package testesunitarios;
+
+import org.junit.Test;
+import testeunitario.Conta;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContaTest {
+    Double saldo = 1000.00;
+    Double taxaOperacao = 0.50;
+    Double saldoFinal= 799.50;
+
+    @Test
+    public void sacarTest(){
+        Conta conta = new Conta(saldo, taxaOperacao);
+        conta.sacar(200.00);
+        assertEquals(saldoFinal, conta.getSaldo());
+    }
+}
